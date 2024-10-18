7114f1c85d077e5f822cb6ed4bf5c9ab1a0147d7
SonyaBurg <sonyaburg@gmail.com>
2024-10-18 19:39:55 +0300

    task02

---
+++ Practice02/src/build/task01/build/lib/task01/reciever.py
@@ -0,0 +1,19 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+class reciever(Node) :
+	def __init__(self) :
+	   super(reciever, self).__init__('simple_node')
+	   timer_period = 1
+	   self.subscriber = self.create_subscription(String, '/spgc/sender', self.subscriber_callback, qos_profile=10)
+
+	def subscriber_callback(self, msg):
+	   self.get_logger().info(msg.data)
+def main () :
+   rclpy.init()
+   node = reciever()
+   rclpy.spin(node)
+   rclpy.shutdown()
+
+if __name__ == '__main__':
+   main()
---
+++ Practice02/src/task02/build/task02/build/lib/task02/publisher.py
@@ -0,0 +1,25 @@
+import rclpy
+from std_msgs.msg import String
+from rclpy. node import Node
+
+class Publisher(Node) :
+	def __init__ ( self ) :
+	   super(Publisher, self).__init__("publisher")
+	   self.declare_parameter("topic_name", "")
+	   self.declare_parameter("text", "Hello, ROS2!")
+	   self.topic_name = self.get_parameter("topic_name").get_parameter_value().string_value
+	   self.publisher = self.create_publisher(String, self.topic_name, 10)
+
+	def timer_callback (self):
+	   msg = String()
+	   msg.data = self.get_parameter("text").get_parameter_value().string_value
+	   self.publisher.publish(msg)
+
+
+def main():
+	rclpy.init()
+	node = Publisher()
+	rclpy.spin(node)
+	rclpy.shutdown()
+if __name__ == '__main__':
+	main()
