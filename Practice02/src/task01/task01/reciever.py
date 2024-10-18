b832f89ba7bee045c28665ce44d492fa665a60d4
SonyaBurg <sonyaburg@gmail.com>
2024-10-18 17:57:34 +0300

    output corrected

--- Practice02/src/task01/task01/reciever.py
+++ Practice02/src/task01/task01/reciever.py
@@ -5,14 +5,9 @@ class reciever(Node) :
 	def __init__(self) :
 	   super(reciever, self).__init__('simple_node')
 	   timer_period = 1
-	   self.timer = self.create_timer(timer_period, self.timer_callback)
 	   self.subscriber = self.create_subscription(String, '/spgc/sender', self.subscriber_callback, qos_profile=10)
 	def subscriber_callback(self, msg):
-	   self.get_logger().info(f"Recieved: {msg.data}")
-
-	def timer_callback(self) :
-	   self.get_logger().info("Hello world!")
-
+	   self.get_logger().info("msg.data")
 def main () :
    rclpy.init()
    node = reciever()
