c3354c5d42f7177b77a31734a321ccdc8455e156
SonyaBurg <sonyaburg@gmail.com>
2024-10-19 13:35:18 +0300

    task02 params fixed

--- Practice02/src/build/task02/build/lib/task02/publisher.py
+++ Practice02/src/build/task02/build/lib/task02/publisher.py
@@ -6,13 +6,16 @@ class Publisher(Node) :
 	def __init__ ( self ) :
 	   super(Publisher, self).__init__("publisher")
 	   self.declare_parameter("topic_name", "")
-	   self.declare_parameter("text", "Hello, ROS2!")
+	   self.declare_parameter("text", "")
+	   timer_period = 0.5
+	   self.timer = self.create_timer(timer_period, self.timer_callback)
 	   self.topic_name = self.get_parameter("topic_name").get_parameter_value().string_value
 	   self.publisher = self.create_publisher(String, self.topic_name, 10)
 
 	def timer_callback (self):
 	   msg = String()
 	   msg.data = self.get_parameter("text").get_parameter_value().string_value
+	   self.get_logger().info(msg.data)
 	   self.publisher.publish(msg)
 
 
